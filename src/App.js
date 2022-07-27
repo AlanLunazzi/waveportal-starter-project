a6fcdd2bf83d5555d072fcee17542d585f8b6b75
Alan Lunazzi <alann93@gmail.com>
2022-07-27 12:37:21 -0300

    Wording

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    /*
-     * First make sure we have access to window.ethereum
-     */
-    const { ethereum } = window;
+  /*
+   * Just a state variable we use to store our user's public wallet.
+   */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  /*
-   * This runs our function when the page loads.
-   */
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -28,16 +45,15 @@ const App = () => {
         <div className="header">👋 Hey there!</div>
 
         <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool
-          right? Connect your Ethereum wallet and wave at me!
+          I am Alan Lunazzi and i'm testing solidity development thanks to
+          buildspace!
         </div>
 
         <button className="waveButton" onClick={null}>
-          Wave at Me
+          Sign my wall!
         </button>
       </div>
     </div>
   );
 };
-
 export default App;
